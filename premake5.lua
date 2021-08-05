05ee6ba9fade46a679596f5d19ab53168d04e3d5
Cankar001 <info.can-karka@web.de>
2021-08-05 23:44:36 +0200

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "HighLo-Unit"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"include/**.h",
+		"include/**.cpp"
+	}
+	
+	filter "system:linux"
+		systemversion "latest"
+
+	filter "system:windows"
+		systemversion "latest"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+		
